534dfbb86bc2caafb5551154d2125408b8cf4d13
Jacob Mann <man21018@byui.edu>
2024-11-07 14:32:22 -0700

    register pt1

---
+++ register/script.js
@@ -0,0 +1,87 @@
+count=1;
+function participantTemplate(count) {
+    return `
+    <section class="participant${count}">
+                        <p>Participant ${count}</p>
+                        <div class="item">
+                            <label for="fname"> First Name<span>*</span></label>
+                            <input id="fname${count}" type="text" name="fname" value
+                                required />
+                        </div>
+                        <div class="item activities">
+                            <label for="activity">Activity
+                                #<span>*</span></label>
+                            <input id="activity${count}" type="text" name="activity" />
+                        </div>
+                        <div class="item">
+                            <label for="fee">Fee ($)<span>*</span></label>
+                            <input id="fee${count}" type="number" name="fee" />
+                        </div>
+                        <div class="item">
+                            <label for="date">Desired Date
+                                <span>*</span></label>
+                            <input id="date${count}" type="date" name="date" />
+                        </div>
+                        <div class="item">
+                            <p>Grade</p>
+                            <label for="grade">Grade<span>*</span></label>
+                            <select id="grade${count}" name="grade">
+                                <option selected value disabled
+                                    selected></option>
+                                <option value="1">1st</option>
+                                <option value="2">2nd</option>
+                                <option value="3">3rd</option>
+                                <option value="4">4th</option>
+                                <option value="5">5th</option>
+                                <option value="6">6th</option>
+                                <option value="7">7th</option>
+                                <option value="8">8th</option>
+                                <option value="9">9th</option>
+                                <option value="10">10th</option>
+                                <option value="11">11th</option>
+                                <option value="12">12th</option>
+                            </select>
+                        </div>
+                    </section>`;
+}
+addButton = document.getElementById('add${count}');
+addButton.insertAdjacentHTML('beforebegin', ``);
+
+// Add event listener for addButton
+addButton.addEventListener('click', function() {
+    // Your event handler code here
+    count+=1;
+    addButton.insertAdjacentHTML('beforebegin', `${participantTemplate(count)}`);
+});
+
+
+
+
+function submitForm(event){
+    event.preventDefault();
+    totalFees();
+}
+
+function totalFees() {
+    // the selector below lets us grab any element that has an id that begins with "fee"
+    let feeElements = document.querySelectorAll("[id^=fee]");
+    console.log(feeElements);
+    // querySelectorAll returns a NodeList. It's like an Array, but not exactly the same.
+    // The line below is an easy way to convert something that is list-like to an actual Array so we can use all of the helpful Array methods...like reduce
+    // The "..." is called the spread operator. It "spreads" apart the list, then the [] we wrapped it in inserts those list items into a new Array.
+    ElementsArray = [...feeElements];
+    console.log(ElementsArray[0].value);
+    // sum up all of the fees. Something like Array.reduce() could be very helpful here :) Or you could use a Array.forEach() as well.
+    // Remember that the text that was entered into the input element will be found in the .value of the element.
+    
+    // once you have your total make sure to return it!
+    // return total
+    }
+
+
+
+formSubmit = document.getElementById('submitButton')[0];
+formSubmit.addEventListener('click', function() {
+    submitForm('submit');
+    
+});
